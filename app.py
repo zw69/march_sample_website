1ac09192fb6650903ed99ba02302ec1f05946055
zw69 <112047566+zw69@users.noreply.github.com>
2024-04-02 02:57:09 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #print("Hello World")
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-  return "Hello World!"
+  return render_template('home.html')
 print(__name__)
 if __name__=="__main__" : 
   app.run(host='0.0.0.0', debug=True)
